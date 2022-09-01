cb42323ef86b4bfbb5a19c790ce3c5d0c4f1690d
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-01 15:02:15 +0600

    search button working poperly

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
-const loadData = async() => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadData = async(searchText) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   const res = await fetch(url);
   const data = await res.json();
   displayPhones(data.data);
 }
 
+
 const displayPhones = phones => {
   console.log(phones);
+  // phones = phones.slice(0,20);
   const phoneContainer = document.getElementById('phone-container');
-  
+  phoneContainer.innerHTML = ``
   phones.forEach(phone => {
-    console.log(phone);
+    console.log(phone)
     const mobileDiv = document.createElement('div');
     mobileDiv.classList.add('col');
     mobileDiv.innerHTML = `
-    
-      <div class="card">
+      <div class="card p-3">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
+        <h5 class="card-title">${phone.brand}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
@@ -27,5 +29,14 @@ const displayPhones = phones => {
   })
 }
 
+
+
+const searchBtn = document.getElementById('btn-search');
+searchBtn.addEventListener('click', function(){
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log('search button clicked', searchText);
+  loadData(searchText);
+})
 
 loadData();
